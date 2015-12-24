c58844ca49300e9792e11995f297c098cfc8a4cb
pplinlin2 <pplinlin2@gmail.com>
2015-12-24 18:14:17 +0800

    move into a function

--- templite.py
+++ templite.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
-context = {'name': 'Jair', 'date': 'Thursday'}
+def render_function(context):
+	result = []
+	c_name = context['name']
+	c_date = context['date']
+	append_result = result.append
+	extend_result = result.extend
+	append_result('Hello, ')
+	append_result(c_name)
+	append_result('. Today is ')
+	append_result(c_date)
 
-result = []
-c_name = context['name']
-c_date = context['date']
-append_result = result.append
-extend_result = result.extend
-append_result('Hello, ')
-append_result(c_name)
-append_result('. Today is ')
-append_result(c_date)
+	return ''.join(result)
 
-print ''.join(result)
+print render_function({'name': 'Jair', 'date': 'Thursday'})
